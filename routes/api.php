e9589ff4eab7d2831192b304e23a718fe82e69a6
Benjamin Fransson <benjamin@cafcon.se>
2019-10-20 22:38:47 +0200

    Restructured routes

--- routes/api.php
+++ routes/api.php
@@ -19,20 +19,24 @@ Route::group(['middleware' => ['json.response']], function () {
     // public routes
     Route::post('/login', 'Api\AuthController@login')->name('login.api');
     Route::post('/register', 'Api\AuthController@register')->name('register.api');
-
-    // private routes
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::middleware('auth:api')->group(function () {
-        Route::get('/logout', 'Api\AuthController@logout')->name('logout');
-    });
-
     Route::get('/test', function (Request $request) {
         return [
             'status' => 200,
             'test' => 'succsessful'
         ];
     });
+
+    // private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'Api\AuthController@logout')->name('logout');
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::get('/auth/test', function (Request $request)
+        {
+            return ['Auth' => 'Successful', 'Description' => 'If this message reached your client, it  means the authorisation worked!'];
+        });
+    });
+
 });
 
