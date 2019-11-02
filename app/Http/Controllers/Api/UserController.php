037f7aa37f3ccc2b246183806b338c143595aee9
Benjamin Fransson <benjamin@cafcon.se>
2019-11-02 15:39:51 +0100

    Added setStatus method to user controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
 
     public function user(Request $request)
     {
@@ -15,5 +17,34 @@ class UserController extends Controller
         $tmp = $user->roles[0];
 
         return $user;
+    }
+
+    public function setStatus(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'status' => [
+                'required',
+                'string',
+                'max:16',
+                Rule::in(['online', 'busy', 'on_break', 'offline']),
+            ],
+        ]);
+
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        $user = Auth::user();
+        $response = ['status' => $request['status']];
+
+        if($user->status === $request['status']){
+            return response($response, 304);
+        }
+
+        $user->status = $request['status'];
+        $user->save();
+
+        return response($response, 200);
     }
 }
