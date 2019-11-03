cec2c8901a9730ade509a6a61857dac824c61357
Benjamin Fransson <benjamin@cafcon.se>
2019-11-03 21:33:05 +0100

    Implemented HandleCors middleware to allow CORS

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\Cors\HandleCors;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +15,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes();
+        Broadcast::routes(['middleware' => [HandleCors::class]]);
 
         require base_path('routes/channels.php');
     }
