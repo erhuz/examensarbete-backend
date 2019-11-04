44162dce39b2182bba455627959d64b17ccef0c3
Benjamin Fransson <benjamin@cafcon.se>
2019-11-04 20:06:45 +0100

    Events now implements ShouldBroadcast

--- app/Events/CallAccepted.php
+++ app/Events/CallAccepted.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class CallAccepted
+class CallAccepted implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
