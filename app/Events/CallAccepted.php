6ca68ac1bdd0d55285f1acedf17bdfdd2d9096d6
Benjamin Fransson <benjamin@cafcon.se>
2019-11-04 16:27:33 +0100

    Added CallAccepted event

---
+++ app/Events/CallAccepted.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Call;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CallAccepted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $call;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Call $call)
+    {
+        $this->call = $call;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('App.User.' + $this->call->caller_id);
+    }
+}
